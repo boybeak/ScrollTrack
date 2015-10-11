d1041756c82e85be5f062268a31fc33ce6c7e7b0
boybeak <boybeak@gmail.com>
2015-10-11 22:06:45 +0800

    add view source menu

--- ScrollTrack/app/src/main/java/com/beak/scrolltrack/MainActivity.java
+++ ScrollTrack/app/src/main/java/com/beak/scrolltrack/MainActivity.java
@@ -1,13 +1,19 @@
 package com.beak.scrolltrack;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.beak.scrolltrack.scroll.BottomTrackListener;
 import com.beak.scrolltrack.scroll.TopDecoration;
@@ -32,6 +38,27 @@ public class MainActivity extends AppCompatActivity {
         mMainRecyclerView.setAdapter(new SimpleAdapter());
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main_check_resource_code:
+                try {
+                    Intent it = new Intent(Intent.ACTION_VIEW);
+                    it.setData(Uri.parse("https://github.com/boybeak/ScrollTrack"));
+                    startActivity(it);
+                } catch (Exception e) {
+                    Toast.makeText(this, "no browser found for view code web page", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private class SimpleAdapter extends RecyclerView.Adapter<SimpleHolder> {
 
